5385378a6d68981f84be440294c16cd086639d42
Shahzeb Javed <shahzebjaved23@gmail.com>
2018-07-04 02:03:59 +0500

    adds the jsx for page selector for footer

--- src/table/footer/Footer.js
+++ src/table/footer/Footer.js
@@ -11,13 +11,24 @@ export class Footer extends Component {
 	render(){
 		return (
 			<div className="footer-div">
-					
+				
 				<span> Items per Page </span>
 				<div className="footer-select">
 					<span className="select-number">10</span>
-					<span className="fa fa-angle-down selector-control"></span>
+					<span className="fa fa-angle-down cursor-pointer"></span>
 				</div>
 				<span className="item-description"> 1 - 10 of 40 items </span>
+
+				<div className="left-arrow">
+					<span className="fa fa-angle-down cursor-pointer"></span>
+				</div>
+				<div className="page-selector">
+					<span className="page-selector-number">10</span>
+					<span className="fa fa-angle-down cursor-pointer"></span>
+				</div>
+				<div className="right-arrow">
+					<span className="fa fa-angle-down cursor-pointer"></span>
+				</div>
 					
 			</div>
 		)

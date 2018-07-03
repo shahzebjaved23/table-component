b47c66b3cc2fd64bf787765603edc171cbcaada8
Shahzeb Javed <shahzebjaved23@gmail.com>
2018-07-03 19:55:56 +0500

    completed the search bar

--- src/table/search-bar/SearchBar.js
+++ src/table/search-bar/SearchBar.js
@@ -6,10 +6,36 @@ export class SearchBar extends Component {
 	render(){
 		return (
 			<div className="search-bar">
-				<div className="search-input-div">
-					<span className="fa fa-search"></span>
-					<input className="search-input" placeholder="Search By Keyword..." />
-				</div>
+				
+				<div className="row">
+					<div className="col-md-2">
+						<div className="search-input-div">
+							<span className="fa fa-search"></span>
+							<input className="search-input" placeholder="Search By Keyword..." />
+						</div>
+					</div>
+					
+					<div className="col-md-2">
+						<div className="rules-selector">
+							<div>
+								<span className="fa fa-angle-down"></span>
+								<span className="fa fa-search"></span>
+								<input className="rules-selector-input" placeholder="View All Rules" />
+							</div>
+						</div>
+					</div>
+
+					<div className="col-md-6"></div>
+					
+					<div className="col-md-2"> 
+						<button className="btn btn-success add-rules-button"> Add Ruleset </button>
+					</div>
+				</div>	
+
+				
+
+				
+
 			</div>
 		)
 	}

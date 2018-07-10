5c2e32ef35eb5cf5d80ec4fab07b63fd95b0e171
Shahzeb Javed <shahzebjaved23@gmail.com>
2018-07-10 14:24:38 +0500

    sort and search fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react';
+import { Footer } from "./footer/Footer";
+import { DisplayTable } from "./display-table/DisplayTable";
+import { SearchBar } from "./search-bar/SearchBar";
+import * as  EventEmitter from "event-emitter";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+export class Table extends Component {
+
+	constructor(props){
+		super(props);
+		this.eventEmitter = EventEmitter();
+		this.state = { tableData: props.data };
+	}
+
+	render(){
+		let eventEmitter = EventEmitter.default();
+		return (
+			<div>
+				<SearchBar eventEmitter={eventEmitter} tableData={this.state.tableData} searchBar={this.props.searchBar} />
+				<DisplayTable eventEmitter={eventEmitter} tableData={this.state.tableData} editButtons={this.props.editButtons} />
+				<Footer eventEmitter={eventEmitter} tableData={this.state.tableData} footer={this.props.footer} />
+			</div>
+		)	
+	}
+}
